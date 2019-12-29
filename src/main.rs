c2e9dbb78e6f8326ee51441fc432699f3bc54918
Corey Farwell <coreyf@rwell.org>
2019-12-28 17:50:22 -0800

    refactor; print dot on success

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,49 @@
+use std::{
+    fs,
+    io::{self, Read, Write},
+};
+
 mod exactpred;
 
 fn main() {
-    let p1_x = 0.0f64;
-    let p1_y = 0.0f64;
-    let p2_x = 100.0f64;
-    let p2_y = 100.0f64;
-    let p3_x = 50.0f64;
-    let p3_y = 50.0f64;
+    loop {
+        run_once()
+    }
+}
+
+fn run_once() {
+    let (p1_x, p1_y) = (rand_f64(), rand_f64());
+    let (p2_x, p2_y) = (rand_f64(), rand_f64());
+    let (p3_x, p3_y) = (rand_f64(), rand_f64());
+
+    let left = run_robust2d(p1_x, p1_y, p2_x, p2_y, p3_x, p3_y);
+    let right = run_exactpred(p1_x, p1_y, p2_x, p2_y, p3_x, p3_y);
+
+    if left.is_nan() && right.is_nan() {
+        return;
+    }
 
     assert_eq!(
-        run_robust2d(p1_x, p1_y, p2_x, p2_y, p3_x, p3_y),
-        run_exactpred(p1_x, p1_y, p2_x, p2_y, p3_x, p3_y),
+        left, right,
         "({}, {}) ({}, {}) ({}, {})",
-        p1_x,
-        p1_y,
-        p2_x,
-        p2_y,
-        p3_x,
-        p3_y
+        p1_x, p1_y, p2_x, p2_y, p3_x, p3_y
     );
+
+    print_dot();
+}
+
+fn print_dot() {
+    print!(".");
+    io::stdout().flush().expect("could not flush stdout");
+}
+
+fn rand_f64() -> f64 {
+    let mut urandom_file = fs::File::open("/dev/urandom").expect("could not open /dev/urandom");
+    let mut bytes = [0u8; 8];
+    urandom_file
+        .read_exact(&mut bytes)
+        .expect("could not read bytes from /dev/urandom");
+    f64::from_be_bytes(bytes)
 }
 
 fn run_robust2d(p1_x: f64, p1_y: f64, p2_x: f64, p2_y: f64, p3_x: f64, p3_y: f64) -> f64 {
